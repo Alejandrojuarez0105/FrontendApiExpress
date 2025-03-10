a94d757ad3736acc31f34b9c6e8170532bd02fc5
Alejandro Juarez <alejandrojuarez01050812@gmail.com>
2025-03-10 17:08:47 +0100

    Inicio de Login

---
+++ login-app/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Importar el nuevo paquete
+import App from './App';
+import './styles/App.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement // Asegúrate de que 'root' sea de tipo HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
